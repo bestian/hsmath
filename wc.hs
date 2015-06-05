8f82fdb7060da545974121cdbf63bbacd06fb6ae
Bestian Tang <besitan@gmail.com>
2015-06-06 06:36:57 +0800

    linear eqution of specific form

--- wc.hs
+++ wc.hs
@@ -6,6 +6,6 @@ check s = case compare (length s) 50 of
 
 main :: IO ()
 main  =  do 
-		f <- readFile "./input.txt"
-		writeFile "./output.txt" (f++"\n\n"++(show.length) f++check f++"字")
+		f <- readFile "wc/input.txt"
+		writeFile "wc/output.txt" (f++"\n\n"++(show.length) f++check f++"字")
 		print $ lines (f++(show.length) f)
